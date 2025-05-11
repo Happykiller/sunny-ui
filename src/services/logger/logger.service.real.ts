9ca38e4d0f580475db5f4ef116cde9983394a73d
Fabrice Rosito <fabrice.rosito@gmail.com>
2025-05-11 18:41:56 +0200

    fix LoggerServiceReal

--- src/services/logger/logger.service.real.ts
+++ src/services/logger/logger.service.real.ts
@@ -2,18 +2,21 @@
 import LoggerService from './logger.service';
 
 export class LoggerServiceReal implements LoggerService {
+
+  constructor(readonly config?: any) {}
+
   log(...args: any[]): void {
     console.log(args);
   }
 
   debug(...args: any[]): void {
-    if (process.env.DEBUG === 'true') {
+    if (this.config?.debug === 'true') {
       console.debug(args);
     }
   }
 
   error(...args: any[]): void {
-    if (process.env.DEBUG === 'true') {
+    if (this.config?.debug === 'true') {
       console.error(args);
     }
   }
