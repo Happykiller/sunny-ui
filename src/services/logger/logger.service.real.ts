3141879e8171410cc41ac5cefb172dc2bfe9343b
f.rosito@thomyris.com <f.rosito@thomyris.com>
2025-06-04 09:05:07 +0200

    fix chatgtp

--- src/services/logger/logger.service.real.ts
+++ src/services/logger/logger.service.real.ts
@@ -5,16 +5,28 @@ export class LoggerServiceReal implements LoggerService {
 
   constructor(readonly config?: any) {}
 
+  /**
+   * For log
+   * @param args 
+   */
   log(...args: any[]): void {
     console.log(...args);
   }
 
+  /**
+   * For debug
+   * @param args 
+   */
   debug(...args: any[]): void {
     if (this.config?.debug === 'true') {
       console.debug(...args);
     }
   }
 
+  /**
+   * For error
+   * @param args 
+   */
   error(...args: any[]): void {
     if (this.config?.debug === 'true') {
       console.error(...args);
